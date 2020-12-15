0dc0ea64d84db67160abf19eeff6b292b4c18ec3
Clément Pit-Claudel <clement.pitclaudel@live.com>
2020-12-14 21:13:01 -0500

    recipes: Add a Lean example

---
+++ recipes/plain.lean
@@ -0,0 +1,14 @@
+#check nat
+
+example (p q r : Prop) : p ∧ q ↔ q ∧ p :=
+begin
+  apply iff.intro,
+  intro H,
+  apply and.intro,
+  apply (and.elim_right H),
+  apply (and.elim_left H),
+  intro H,
+  apply and.intro,
+  apply (and.elim_right H),
+  apply (and.elim_left H),
+end
