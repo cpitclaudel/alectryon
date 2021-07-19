da1dba8f650660037349c0f35a6e1c299f63561a
Clément Pit-Claudel <clement.pitclaudel@live.com>
2021-07-19 16:45:24 -0400

    latex: Preserve labels on alectryon-io blocks

--- alectryon/latex.py
+++ alectryon/latex.py
@@ -241,6 +241,8 @@ class LatexGenerator:
         # FIXME: classes. optargs=[",".join(classes)] if classes else [] ?
         with environments.alectryon() as env:
             Raw("% Generator: {}".format(GENERATOR))
+            for name in ids:
+                macros.label(name)
             # fragments = transforms.merge_fragments_by_line(fragments) # FIXME
             fragments = transforms.group_whitespace_with_code(fragments)
             fragments = transforms.commit_io_annotations(fragments)#, discard_folded=True)
