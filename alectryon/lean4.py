c6dd873c9532154b93ba916d28c6f3301f5f585a
Niklas Bülow <me@niklasbuelow.com>
2021-11-16 01:44:06 +0100

    lean4: Decode LeanInk result

--- alectryon/lean4.py
+++ alectryon/lean4.py
@@ -3,7 +3,10 @@
 import tempfile
 import os
 from pathlib import Path
-from .core import CLIDriver, EncodedDocument, Goal, Hypothesis, Message, Sentence, Text
+from alectryon import json
+
+from alectryon.json import PlainSerializer
+from .core import CLIDriver, EncodedDocument, Goal, Hypothesis, Message, Sentence, Text, UnexpectedError
 
 class Lean4(CLIDriver):
     BIN = "leanInk"
@@ -17,17 +20,39 @@ class Lean4(CLIDriver):
     CLI_ARGS = ("analyze",)
 
     TMP_PREFIX = "leanInk_"
+    LEAN_FILE_EXT = ".lean"
+    LEAN_INK_FILE_EXT = ".leanInk"
 
-    def run_leanInk(self, encoded_document):
+    def run_leanInk_document(self, encoded_document):
+        r""""
+        Run LeanInk with encoded_document file.
+        """
         with tempfile.TemporaryDirectory(prefix=self.TMP_PREFIX) as working_directory:
-            source = Path(working_directory) / os.path.basename(self.fpath)
-            source.write_bytes(encoded_document.contents)
-            result = self.run_cli(more_args=[str(source)])
-
+            inputFile = Path(working_directory) / os.path.basename(self.fpath)
+            inputFile.write_bytes(encoded_document.contents)
+            result = self.run_cli(more_args=[str(inputFile)])
             print(result)
+            outputFile = inputFile.with_suffix(self.LEAN_FILE_EXT + self.LEAN_INK_FILE_EXT)
+            content = outputFile.read_text(encoding="utf-8")
+            print(content)
+            jsonResult = json.loads(content)
+            tupleResult = PlainSerializer.decode(jsonResult)
+            print(tupleResult)
+            return tupleResult
+
+    def run_leanInk_filePath(self, fpath):
+        r""""
+        Run LeanInk with file at filePath.
+        """
+        fileExt = fpath.suffix
+
+        if fileExt == self.LEAN_FILE_EXT:
+            result = self.run_cli(more_args=[str(fpath)])
+            print(result)
+        else:
+            raise UnexpectedError("Filepath provided to Lean 4 driver is not a valid .lean file!")
 
     def annotate(self, chunks):
         document = EncodedDocument(chunks, "\n", encoding="utf-8")
         string = document.contents.decode()
-        self.run_leanInk(document) # Actually use the info from LeanInk to annotate chunks
-        return [[Sentence(string, messages=[Message("Message")], goals=[Goal("Goal", "Conclusion", [Hypothesis("h", "Body", "Type")])])]] # TODO: Actually return something useful
+        return [self.run_leanInk_document(document)] # Atm this only works for a single chunk, we have to split them up again after evaluation.
