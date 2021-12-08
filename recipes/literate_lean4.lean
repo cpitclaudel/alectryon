5bc29cdbd99863e786da20ba37dbed7ed485f0cb
Niklas Bülow <me@niklasbuelow.com>
2021-12-08 21:26:43 +0100

    lean4: add literate_lean4.lean recipe

---
+++ recipes/literate_lean4.lean
@@ -0,0 +1,63 @@
+/-|
+==================================================
+ Literate programming with Alectryon (Lean4 input)
+==================================================
+
+Alectryon supports literate programs and documents (combinations of code and prose) written in Lean4 and reStructuredText.  Here is an example written in Lean4.  It can be converted to reST, HTML, or LaTeX using the following commands::
+
+   alectryon literate_lean4.lean
+       # Coq+reST → HTML;  produces ‘literate_lean4.html’
+   alectryon literate_lean4.lean --backend latex \
+        --latex-dialect xelatex \
+        -o literate_lean4.xe.tex
+       # Coq+reST → LaTeX; produces ‘literate_lean4.xe.tex’
+   alectryon literate_lean4.lean --backend rst
+       # Coq+reST → reST;  produces ‘literate_lean4.lean.rst’
+
+-----
+
+.. default-role:: lean4
+
+Running queries
+===============
+
+Alectryon captures the results of `#check`, `#eval`, and the like:
+|-/
+
+def x : Nat := 5
+#reduce 5 + x
+
+/-|
+By default, these results are folded and are displayed upon hovering or clicking.  We can unfold them by default using annotations or directives:
+|-/
+
+#check Nat /- .unfold -/
+
+/-|
+.. lean4:: unfold
+|-/
+
+#check Bool
+#eval 1 + 1
+
+/-| Other flags can be used to control display, like ``.no-in``: |-/
+
+#print Iff /- .unfold .no-in -/
+
+/-|
+Documenting proofs
+==================
+
+Alectryon also captures goals and hypotheses as proofs progress:
+|-/
+
+theorem test (p q : Prop) (hp : p) (hq : q): p ∧ q ↔ q ∧ p := by
+  apply Iff.intro
+  . intro h
+    apply And.intro
+    . exact hq
+    . exact hp
+  . intro h
+    apply And.intro
+    . exact hp
+    . exact hq
