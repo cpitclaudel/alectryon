bc96e6e86c07c3db055fe56d7c709304200376b1
Niklas Bülow <me@niklasbuelow.com>
2022-04-03 21:56:41 +0200

    lean4: adjust Lean 4 literate opening token
    Changes the literate opening token from `/-|` to `/-!`.

--- alectryon/literate.py
+++ alectryon/literate.py
@@ -865,8 +865,8 @@ def rst2lean3(rst):
 LEAN4 = LangDef(
     "lean4",
     LeanParser, # We can use the same parser as Lean 3, because the syntax for comments has not changed between the versions.
-    lit_open=r"/-|", lit_close=r"-/",
-    lit_open_re=r"[/][-][|][ \t]*", lit_close_re=r"[ \t]*?[-][/]\Z",
+    lit_open=r"/-!", lit_close=r"-/",
+    lit_open_re=r"[/][-][!][ \t]*", lit_close_re=r"[ \t]*?[-][/]\Z",
     quote_pairs=[("/-", r"/\ -"), ("-/", r"-\ /")]
 )
 
