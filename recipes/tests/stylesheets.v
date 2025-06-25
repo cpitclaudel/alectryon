bcb38184d461ace087b5b7be28d2fe1b31e02cea
Clément Pit-Claudel <clement.pit-claudel@epfl.ch>
2025-06-25 15:24:05 +0200

    tests: Update output of LaTeX tests

--- recipes/tests/stylesheets.v
+++ recipes/tests/stylesheets.v
@@ -7,13 +7,13 @@ To compile::
 
    $ DOCUTILSCONFIG=tests/stylesheets.docutils.conf \
        alectryon stylesheets.v --pygments-style emacs -o - \
-       | sed -r '/^ *<style type="text.css">/,/^ *<.style>/ { /^ *<style |<.style>|Alectryon/b; d}' \
+       | sed -r '/^ *<style type="text.css">/,/^ *<.style>/ { /^ *<style |<.style>/b; d}' \
        > stylesheets.html
      # reST → HTML; produces ‘stylesheets.html’
 
    $ DOCUTILSCONFIG=tests/stylesheets.docutils.conf \
        alectryon stylesheets.v --pygments-style emacs --backend latex -o - \
-       | sed -r '/^% embedded stylesheet/,/^\\makeatother/ { /^\\makeat|Alectryon/b; d}' \
+       | sed -r '/^% embedded stylesheet/,/^\\makeatother/ { /^\\makeat/b; d}' \
        > stylesheets.part.tex
      # reST → LaTeX; produces ‘stylesheets.part.tex’
 
