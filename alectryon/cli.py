e95fa7b7a8f7c45c5c6b057fe8e89252a8edbeb9
MathisBD <mathis.bouverot@ens.psl.eu>
2025-06-30 10:03:59 +0200

    add support for coq-lsp

--- alectryon/cli.py
+++ alectryon/cli.py
@@ -845,6 +845,7 @@ DRIVER_ARGS_BY_NAME: Dict[str, Optional[str]] = {
     "sertop": "sertop_args",
     "sertop_noexec": "sertop_args",
     "coqc_time": "coqc_args",
+    "coq_lsp": None,
     "lean3_repl": None,
     "leanInk": "leanInk_args",
     "vscoq": None
