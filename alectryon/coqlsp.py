78acaa6a6af2f3067ceee899d8653204a29cff25
Clément Pit-Claudel <clement.pit-claudel@epfl.ch>
2025-07-06 22:28:38 +0200

    coqlsp: Rebase on top of revised LSP API

--- alectryon/coqlsp.py
+++ alectryon/coqlsp.py
@@ -1,107 +1,126 @@
-from typing import Any, Dict, Iterator, Iterable, List, Tuple, Union
+# Copyright © 2025 Mathis Bouverot, Clément Pit-Claudel
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-import .core
-from .lspFinal import LSPClient, LSPRequest
-from .core import REPLDriver, debug, Hypothesis, Goal, \
-    Sentence, Text, Fragment, Message, Document, Positioned
+from typing import Iterable
 
-class CoqLSPClient(LSPClient):
-    CLIENT_NAME = "Alectryon"
-    LANGUAGE_ID = "coq"
+import dataclasses
 
+from .core import Document, EncodedDocument, Fragment, Goal, Hypothesis, Message, Position, Positioned, Sentence, must
+from .lsp import LSPClient, LSPClientRequest, LSPDriver
+
+class Requests:
+    GOALS = "proof/goals"
+
+@dataclasses.dataclass
+class CoqGetDocumentRequest(LSPClientRequest):
+    METHOD = "coq/getDocument"
+    uri: str
+
+    @property
+    def params(self):
+        return { "textDocument": { "uri": self.uri, "version": 0 } }
+
+@dataclasses.dataclass
+class ProofGoalsRequest(LSPClientRequest):
+    METHOD = "proof/goals"
+    uri: str
+    pos: Position
+
+    @property
+    def params(self):
+        return { "textDocument": { "uri": self.uri, "version": 0 },
+                 "position": self.pos.to_lsp() }
+
+class CoqLSPOutput:
     @staticmethod
     def _decode_hyp(hyp):
         return Hypothesis(hyp["names"], hyp["def"], hyp["ty"])
 
     @staticmethod
     def _decode_hyps(hyps):
-        return [CoqLSPClient._decode_hyp(h) for h in hyps]
+        return [CoqLSPOutput._decode_hyp(h) for h in hyps]
 
     @staticmethod
     def _decode_goal(goal):
         return Goal(goal["info"]["name"], goal["ty"],
-            CoqLSPClient._decode_hyps(goal["hyps"]))
+                    CoqLSPOutput._decode_hyps(goal["hyps"]))
 
     @staticmethod
-    def _decode_goals(goals):
-        return [CoqLSPClient._decode_goal(g) for g in goals]
+    def decode_goals(goals):
+        return [CoqLSPOutput._decode_goal(g) for g in goals]
 
     @staticmethod
     def _decode_message(msg):
-        """Messages can be either a string or an object which contains a string."""
-        if isinstance(msg, str):
-            return Message(msg)
-        else:
-            return Message(msg["text"])
+        # Messages can be either a string or an object which contains a string.
+        return Message(msg if isinstance(msg, str) else msg["text"])
 
     @staticmethod
-    def _decode_messages(msgs):
-        return [CoqLSPClient._decode_message(m) for m in msgs]
+    def decode_messages(msgs):
+        return [CoqLSPOutput._decode_message(m) for m in msgs]
+
+class CoqLSPFile:
+    def __init__(self, driver: "CoqLSP", doc: Document):
+        self.client = must(driver.client)
+        self.observer = driver.observer
+        self.fpath, self.uri = driver.fpath, driver.uri
+        self.doc = doc
 
     def _get_ranges(self):
         """Segment the document into a list of ranges (beginning/end) for each sentence."""
-        request = LSPRequest(
-            self.get_next_request_id(),
-            "coq/getDocument",
-            {"textDocument": {"uri": self.uri, "version": 0}})
-        self.send_and_process(request)
-        return [(span["range"]["start"], span["range"]["end"]) for span in request.result["spans"]]
+        json = must(CoqGetDocumentRequest(self.client, self.uri).send().result)
+        return [(span["range"]["start"], span["range"]["end"]) for span in json["spans"]]
 
-    def _get_sentence(self, text, position):
-        """Get the sentence at a given position.
-        The position should be in the range [sentence_start, sentence_end[
-        i.e. start included and end excluded.
-        """
-        request = LSPRequest(
-            self.get_next_request_id(),
-            "proof/goals",
-            {"textDocument": {"uri": self.uri, "version": 0},
-             "position": position })
-        self.send_and_process(request)
-        messages = CoqLSPClient._decode_messages(request.result["messages"])
-        if "goals" in request.result:
-            goals = CoqLSPClient._decode_goals(request.result["goals"]["goals"])
-        else:
-            goals = []
-        return Sentence(text, messages, goals)
+    def _get_sentence(self, js_beg, js_end) -> Positioned[Sentence]:
+        # According to Emilio we shouldn't trust the "offset" field of positions
+        # returned by coq-lsp.
+        beg, end = Position.of_lsp(self.fpath, js_beg), Position.of_lsp(self.fpath, js_end)
+        beg_ofs, end_ofs = self.doc.pos2offset(beg), self.doc.pos2offset(end)
+        info = must(ProofGoalsRequest(self.client, self.uri, beg).send().result)
+        messages = CoqLSPOutput.decode_messages(info["messages"])
+        goals = CoqLSPOutput.decode_goals(g["goals"]) if (g := info.get("goals")) else []
+        return Positioned(beg_ofs, end_ofs, Sentence(self.doc[beg_ofs:end_ofs], messages, goals))
 
-    def process(self, uri: str, document: Document) -> List[Fragment]:
-        self.uri = uri
-        # Initialize.
-        self.initialize()
-        self.open_document(self.uri, document.contents)
-        # Segment the document.
-        ranges = self._get_ranges()
-        # Get a sentence for each segment.
-        sentences = []
-        for (start, end) in ranges:
-            # According to Emilio we shouldn't trust the "offset" field of positions
-            # returned by coq-lsp.
-            start_ofs = document.pos2offset(start["line"] + 1, start["character"])
-            end_ofs = document.pos2offset(end["line"] + 1, end["character"])
-            text = document.contents[start_ofs : end_ofs]
-            s = self._get_sentence(text, start)
-            # Coq-lsp adds an empty sentence at the end.
-            # We simply remove empty sentences.
-            if s.contents:
-                sentences.append(Positioned(start_ofs, end_ofs, s))
-        # Intersperse the sentences in the original document.
-        return document.intersperse_text_fragments(sentences)
+    def _get_sentences(self) -> Iterable[Positioned[Sentence]]:
+        for (beg, end) in self._get_ranges():
+            sentence = self._get_sentence(beg, end)
+            if sentence.e.contents: # Coq-lsp adds an empty sentence at the end.
+                yield sentence
 
-class CoqLSP(REPLDriver):
+    def process(self) -> Iterable[Positioned[Fragment]]:
+        self.client.open(self.uri, self.doc.str)
+        return self._get_sentences()
+
+class CoqLSPClient(LSPClient):
+    LANGUAGE_ID = "coq"
+
+class CoqLSP(LSPDriver[CoqLSPClient]):
+    ID = "coq-lsp"
     BIN = "coq-lsp"
-    NAME = "Coq LSP Server"
+    NAME = "Coq LSP"
 
-    CLI_ARGS = ()
-    REPL_ARGS = ()
     VERSION_ARGS = ("--version",)
 
-    ID = "coq_lsp"
-    LSP_LANGUAGE_ID = LANGUAGE = "coq"
+    CLIENT = CoqLSPClient
 
-    def annotate(self, chunks: Iterable[str]) -> List[List[Positioned]]:
-        with self as api:
-            assert api.repl
-            document = Document(chunks, "\n")
-            client = CoqLSPClient(api.repl)
-            return client.process(self.fpath.absolute().as_uri(), document)
+    def _encode(self, chunks: Iterable[str]) -> Document:
+        return Document(chunks, "\n")  # TODO: UTF-16 positions
+
+    def _find_sentences(self, document: Document) -> Iterable[Positioned[Fragment]]:
+        return CoqLSPFile(self, document).process()
