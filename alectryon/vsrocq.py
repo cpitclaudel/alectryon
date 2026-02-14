cbdafc8d26b2f10116bbb423a2214de5d7eafe91
Enrico Tassi <Enrico.Tassi@inria.fr>
2026-02-14 10:33:07 +0100

    vsrocq: Port to latest release

--- alectryon/vsrocq.py
+++ alectryon/vsrocq.py
@@ -29,12 +29,12 @@ from .lsp import JSON, LSPClient, LSPClientNotification, LSPClientRequest, LSPDi
 from .coq import CoqIdents
 
 class Notifications(LSPServerNotifications):
-    PROOF_VIEW = "vscoq/proofView"
-    BLOCK_ON_ERROR = "vscoq/blockOnError"
+    PROOF_VIEW = "prover/proofView"
+    BLOCK_ON_ERROR = "prover/blockOnError"
 
 @dataclasses.dataclass
 class StepForwardNotification(LSPClientNotification):
-    METHOD = "vscoq/stepForward"
+    METHOD = "prover/stepForward"
     fpath: Path
     uri: str
 
@@ -71,10 +71,10 @@ class URIRequest(LSPClientRequest):
         return { "textDocument": { "uri": self.uri } }
 
 class DocumentProofsRequest(URIRequest):
-    METHOD = "vscoq/documentProofs"
+    METHOD = "prover/documentProofs"
 
 class DocumentStateRequest(URIRequest):
-    METHOD = "vscoq/documentState"
+    METHOD = "prover/documentState"
 
 @dataclasses.dataclass
 class ExponentialBackoff:
@@ -188,10 +188,11 @@ class VsRocqClient(LSPClient):
 class VsRocqFile(LSPFile[VsRocqClient]):
     def _compute_ranges(self):
         req = DocumentStateRequest(self.client, self.uri)
-        document = must(req.send().result)["document"]
-        first_section = document.split("Document using sentences_by_end map")[0]
+        document: str = must(req.send().result)["document"]
+        sep = document.find("Document using sentences_by_end map")
+        assert sep >= 0, "Unexpected VsRocq documentState format"
         PATTERN = re.compile(r"\[\d+\].*?[(](?P<beg>\d+) -> (?P<end>\d+)[)]")
-        for m in PATTERN.finditer(first_section):
+        for m in PATTERN.finditer(document[:sep].replace("\n", " ")):
             yield int(m.group("beg")), int(m.group("end"))
 
     def process(self) -> Iterable[Positioned[Fragment]]:
@@ -232,7 +233,7 @@ class VsRocqFile(LSPFile[VsRocqClient]):
         return f"{prefix}>>>{substring}<<<{suffix}"
 
 class VsRocq(LSPDriver[VsRocqClient]):
-    BIN = "vscoqtop"
+    BIN = "vsrocqtop"
     NAME = "VsRocq"
     VERSION_ARGS = ("--version",)
 
@@ -247,7 +248,7 @@ class VsRocq(LSPDriver[VsRocqClient]):
         return CoqIdents.toppath_of_fpath(p)
 
     def _encode(self, chunks: Iterable[str]) -> Document:
-        # FIXME: VsRocq sends vscoq/documentState info using utf-8 offsets but
+        # FIXME: VsRocq sends prover/documentState info using utf-8 offsets but
         # uses codepoints (instead of utf-16?) in diagnostic line/char pairs.
         return UTF8Document(chunks, "\n")
 
