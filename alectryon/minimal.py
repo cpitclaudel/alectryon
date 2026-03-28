f7002e1a6d138d1036ec24a1cd00290850525820
Clément Pit-Claudel <clement.pit-claudel@epfl.ch>
2026-03-29 00:11:56 +0100

    deps: Use lowest supported Python version in CI

--- alectryon/minimal.py
+++ alectryon/minimal.py
@@ -112,7 +112,7 @@ def cli():
     from docutils.core import publish_cmdline, default_description
     docutils_setup()
     DESCRIPTION = 'Convert reSt + Alectryon to HTML.' + default_description
-    publish_cmdline(writer='html', description=DESCRIPTION)
+    publish_cmdline(writer_name='html', description=DESCRIPTION)
 
 if __name__ == '__main__':
     cli()
