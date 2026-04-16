5b62bc43cf6af209b274ef0077356eaaabe6e5af
Clément Pit-Claudel <clement.pit-claudel@epfl.ch>
2026-04-16 09:52:40 +0200

    build: Add a way to run tests with dynamic type checking

--- recipes/tests/sitecustomize/sitecustomize.py
+++ recipes/tests/sitecustomize/sitecustomize.py
@@ -1,3 +1,12 @@
-"""Set Lean3 binary to "lean3" to avoid conflicts."""
+"""Set up a test environment."""
+
+import os
+
+# Set Lean3 binary to "lean3" to avoid conflicts
 from alectryon.lean3 import Lean3
 Lean3.BIN = "lean3"
+
+# Install the typeguard hook
+if os.environ.get("TYPEGUARD"):
+    from typeguard import install_import_hook
+    install_import_hook("alectryon")
